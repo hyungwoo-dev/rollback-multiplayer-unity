26e303a30ceaaeaaa3c759a548fb907fc6a18eb7
hyungwoo-kwon < guddn5792@gmail.com>
2026-01-10 05:44:32 +0900

    Debug 추가

---
+++ Unity/Assets/Scripts/Common/Debug.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+
+[ManagedStateIgnore]
+public class Debug
+{
+    private const string CONDITION_STRING = "UNITY_EDITOR";
+
+    public static Debug Shared { get; } = new(string.Empty);
+
+    private readonly string _tag;
+
+    public Debug(string tag)
+    {
+        _tag = tag;
+    }
+
+    [Conditional(CONDITION_STRING)]
+    public void Log(object message)
+    {
+        UnityEngine.Debug.Log(GetTagString() + message);
+    }
+
+    [Conditional(CONDITION_STRING)]
+    public void LogWarning(object message)
+    {
+        UnityEngine.Debug.LogWarning(GetTagString() + message);
+    }
+
+    [Conditional(CONDITION_STRING)]
+    public void LogError(object message)
+    {
+        UnityEngine.Debug.LogError(GetTagString() + message);
+    }
+
+    private string GetTagString()
+    {
+        return string.IsNullOrWhiteSpace(_tag) ? string.Empty : $"[{_tag}] ";
+    }
+}
