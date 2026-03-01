461d50fe13bf3325a3316b924ea0a8fb13d53cce
hyungwoo-kwon < guddn5792@gmail.com>
2026-03-01 13:42:48 +0900

    빌드에서도 로그 출력하도록 수정

--- Unity/Assets/Scripts/Common/Debug.cs
+++ Unity/Assets/Scripts/Common/Debug.cs
@@ -1,10 +1,6 @@
-﻿using System.Diagnostics;
-
-[ManagedStateIgnore]
+﻿[ManagedStateIgnore]
 public class Debug
 {
-    private const string CONDITION_STRING = "UNITY_EDITOR";
-
     public static Debug Shared { get; } = new(string.Empty);
 
     private readonly string _tag;
@@ -14,25 +10,21 @@ public class Debug
         _tag = tag;
     }
 
-    [Conditional(CONDITION_STRING)]
     public void Log(object message)
     {
         UnityEngine.Debug.Log(GetTagString() + message);
     }
 
-    [Conditional(CONDITION_STRING)]
     public void LogWarning(object message)
     {
         UnityEngine.Debug.LogWarning(GetTagString() + message);
     }
 
-    [Conditional(CONDITION_STRING)]
     public void LogError(object message)
     {
         UnityEngine.Debug.LogError(GetTagString() + message);
     }
 
-    [Conditional(CONDITION_STRING)]
     public void LogException(System.Exception exception)
     {
         UnityEngine.Debug.LogException(exception);
